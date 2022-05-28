326db262fd5021cce2bef6485d4801779bc618f4
Michael Seifert <m.seifert@digitalernachschub.de>
2022-05-28 09:41:20 +0200

    Initial commit.

---
+++ exercises/tests/test_approaches.py
@@ -0,0 +1,36 @@
+"""
+These exercises focus on different approaches for writing property-based tests.
+
+Try to find more than one test case for each exercise and test the functions under test extensively.
+"""
+import pytest
+from hypothesis import given, strategies as st
+
+# Exercise 1: Use Hypothesis to test the `reversed` built-in function for lists
+
+# Your strategy goes here
+def test_reversed():
+  # Your test goes here
+  pytest.skip()
+
+# Additional test cases go here
+
+
+
+# Exercise 2: Use Hypothesis to test the `sorted` built-in function for lists.
+
+# Your strategy goes here
+def test_sorted():
+  # Your test goes here
+  pytest.skip()
+
+# Additional test cases go here
+
+
+
+# Exercise 3: Use Hypothesis to test the `enumerated` built-in function for lists.
+
+# Your strategy goes here
+def test_enumerated():
+  # Your test goes here
+  pytest.skip()
